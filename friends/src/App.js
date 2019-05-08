b99cb33178320503a696e65146cc00666931aa0d
azinoVo <azinovo22@gmail.com>
2019-05-08 17:11:00 -0500

    data set to state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import FriendList from './components/FriendList';
 
-function App() {
-  return (
-    <div>
-      Friends App
-    </div>
-  );
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    //axios.get returns a promise that we can 
+    // chain .then() and .catch() and .finally()
+    axios.get('http://localhost:5000/friends')
+    .then(response => this.setState({friends: response.data}))
+    .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div>
+      <FriendList />
+      </div>
+    );
+  }
 }
+
 
 export default App;
